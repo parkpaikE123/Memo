dc3510812b7b3ed9a2a379ba33e1148310f67cd2
wjdals2285 <wjdals2285@naver.com>
2025-03-30 05:45:56 +0900

    [류정민] {MEMO프로젝트} - 삭제기능 (이미지파일 포함) 추가

--- src/main/java/com/ryujm/memo/post/service/PostService.java
+++ src/main/java/com/ryujm/memo/post/service/PostService.java
@@ -91,6 +91,8 @@ public class PostService {
 			
 			Post post = optionalPost.get();
 			
+			FileManager.removeFile(post.getImagePath());
+			
 			try {
 				postRepository.delete(post);
 			} catch(PersistenceException e) {
@@ -99,6 +101,7 @@ public class PostService {
 		} else {
 			return false;
 		}
+		
 		return true;
 	}
 	
