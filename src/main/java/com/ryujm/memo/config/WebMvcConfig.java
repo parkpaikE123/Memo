f919f6e280fb82bac151599a07a16230e842077c
wjdals2285 <wjdals2285@naver.com>
2025-03-31 01:28:14 +0900

    [류정민] {MEMO프로젝트 최종 완성} -접근권한 제한 설정

--- src/main/java/com/ryujm/memo/config/WebMvcConfig.java
+++ src/main/java/com/ryujm/memo/config/WebMvcConfig.java
@@ -1,10 +1,12 @@
 package com.ryujm.memo.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.ryujm.memo.common.FileManager;
+import com.ryujm.memo.interceptor.PermissonInterceptor;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
@@ -13,6 +15,15 @@ public class WebMvcConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/images/**")
 		.addResourceLocations("file:///" + FileManager.FILE_UPLOAD_PATH + "/");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(new PermissonInterceptor())
+		.addPathPatterns("/**")
+		.excludePathPatterns("/user/logout"
+							, "/static/**"
+							, "/images/**");
 	}
 	
 	
